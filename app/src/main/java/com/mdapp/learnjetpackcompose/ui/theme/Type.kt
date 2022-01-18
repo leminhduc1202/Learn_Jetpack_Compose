e8db2b7dc8c713f14bab55ddc8cd2cc7f5a3462b
duc.le@madison-technology.com <duc.le@madison-technology.com>
2022-01-18 14:45:26 +0700

    add body2 h1

--- app/src/main/java/com/mdapp/learnjetpackcompose/ui/theme/Type.kt
+++ app/src/main/java/com/mdapp/learnjetpackcompose/ui/theme/Type.kt
@@ -3,6 +3,7 @@ package com.mdapp.learnjetpackcompose.ui.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -12,7 +13,17 @@ val Typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    body2 = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Normal
+    ),
+    h1 = TextStyle(
+        fontStyle = FontStyle.Italic,
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
