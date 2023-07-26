653591486c669949e7c498b6a1056d9b5b2ecd00
LeMinhDucDN <duc.le@madison-technology.com>
2023-07-26 16:52:47 +0700

    add lemon app upgrade build Gradle version

--- app/src/main/java/com/mdapp/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/mdapp/learnjetpackcompose/MainActivity.kt
@@ -4,44 +4,209 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mdapp.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello")
-            Spacer(modifier = Modifier.width(12.dp))
-            LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+//            Text(text = "Hello")
+//            Spacer(modifier = Modifier.width(12.dp))
+//            LearnJetpackComposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+//            }
+//            MessageCard(msg = Message("Duc", "World"))
+//            DiceRollerApp()
+            LemonApp()
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = false)
+@Composable
+fun DiceRollerApp() {
+//    DiceWithButtonAndImage()
+    LemonApp()
+}
+
+
+@Composable
+fun LemonApp() {
+    var currentStep by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(MaterialTheme.colors.primary),
+        color = MaterialTheme.colors.background
+    ) {
+        when (currentStep) {
+            1 -> {
+                LemonTextAndImage(textLabelResourceId = R.string.lemon_select,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        currentStep = 2
+                        squeezeCount = (2..4).random()
+                    })
+            }
+
+            2 -> {
+                LemonTextAndImage(textLabelResourceId = R.string.lemon_squeeze,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        squeezeCount--
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    })
+            }
+
+            3 -> {
+                LemonTextAndImage(textLabelResourceId = R.string.lemon_drink,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.lemonade_content_description,
+                    onImageClick = {
+                        currentStep = 4
+                    })
+            }
+
+            4 -> {
+                LemonTextAndImage(textLabelResourceId = R.string.lemon_empty_glass,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.empty_glass_content_description,
+                    onImageClick = {
+                        currentStep = 1
+                    })
             }
         }
     }
 }
 
+@Composable
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Button(
+                onClick = onImageClick,
+                shape = RoundedCornerShape(dimensionResource(id = R.dimen.button_corner_radius)),
+                colors = ButtonDefaults.buttonColors(contentColor = MaterialTheme.colors.primaryVariant)
+            ) {
+                Image(
+                    painter = painterResource(id = drawableResourceId),
+                    contentDescription = stringResource(contentDescriptionResourceId),
+                    modifier = Modifier
+                        .width(dimensionResource(id = R.dimen.button_image_width))
+                        .height(dimensionResource(id = R.dimen.button_image_height))
+                        .padding(dimensionResource(id = R.dimen.button_interior_padding))
+                )
+            }
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.padding_vertical)))
+            Text(
+                text = stringResource(id = textLabelResourceId),
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+
+@Composable
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+) {
+    var result by remember { mutableStateOf(1) }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column(
+        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResource), contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text(text = stringResource(id = R.string.roll))
+
+        }
+    }
+
+
+}
+
+
+@Composable
+fun DefaultPreview() {
+//    Column {
+//        MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
+//        Divider(modifier = Modifier.fillMaxWidth())
+//    }
+//    LearnJetpackComposeTheme {
+////        Column() {
+////
+////            Greeting(name = "Jackson")
+////            LayoutTopBar()
+//////        SimpleList()
+////        }
+//        LazyList()
+//    }
+
+    MessageCard(Message("Duc", "World"))
+
+}
+
 data class Message(
-    val author: String,
-    val body: String
+    val author: String, val body: String
 )
 
 @Composable
@@ -69,8 +234,7 @@ fun MessageCard(msg: Message) {
             )
             Spacer(modifier = Modifier.height(10.dp))
             Text(
-                text = msg.body,
-                color = Color.Red
+                text = msg.body, color = Color.Red
             )
         }
     }
@@ -145,8 +309,7 @@ fun LazyList() {
                 .height(2.dp)
         )
         LazyColumn(
-            state = scrollState,
-            modifier = Modifier.fillMaxWidth(),
+            state = scrollState, modifier = Modifier.fillMaxWidth(),
 //        verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.Start
         ) {
@@ -158,23 +321,6 @@ fun LazyList() {
 
 }
 
-@Preview(showBackground = false, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-//    Column {
-//        MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!"))
-//        Divider(modifier = Modifier.fillMaxWidth())
-//    }
-    LearnJetpackComposeTheme {
-//        Column() {
-//
-//            Greeting(name = "Jackson")
-//            LayoutTopBar()
-////        SimpleList()
-//        }
-        LazyList()
-    }
 
-}
 
 
